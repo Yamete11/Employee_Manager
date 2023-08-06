9cdb685ba8c51d34b3168c9024b425473fb6d2ae
Gleb Ivanov <s22223@pjwstk.edu.pl>
2023-08-06 23:12:46 +0200

    be file init

---
+++ BE_employeeManager/src/main/java/com/example/be_employeemanager/BeEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeEmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeEmployeeManagerApplication.class, args);
+	}
+
+}
---
+++ BE_employeeManager/src/test/java/com/example/be_employeemanager/BeEmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeEmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
